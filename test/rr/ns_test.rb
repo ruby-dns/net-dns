01409ef9c3ab6ee39388fcdb7baaef76dabde882
Simone Carletti <weppos@weppos.net>
2009-07-28 14:11:08 +0200

    Refactoring RR::NS tests to improve readability and maintanability.

--- test/rr/ns_test.rb
+++ test/rr/ns_test.rb
@@ -4,63 +4,61 @@ require 'net/dns/rr'
 class RRNSTest < Test::Unit::TestCase
   
   def setup
-    @rr_name = "google.com."
-    @type = "NS"
-    @cls = "IN"
-    @ttl = 10800
-    @nsdname = "ns1.google.com."
-
+    @rr_name    = "google.com."
+    @rr_type    = "NS"
+    @rr_cls     = "IN"
+    @rr_ttl     = 10800
+    @rr_nsdname = "ns1.google.com."
     
-    @hash = Net::DNS::RR::NS.new(:name => @rr_name,
-                                 :nsdname => @nsdname)
-    
-    @string = Net::DNS::RR::NS.new("google.com. 10800 IN NS ns1.google.com.")
-    
-    @arr = Net::DNS::RR.parse(@string.data)
-
-    @str = "google.com.             10800   IN      NS      ns1.google.com."
-  end
-    
-  def test_simple
-    assert_equal(@str,@hash.inspect)
-    assert_equal(@rr_name, @hash.name)
-    assert_equal(@type, @hash.type)
-    assert_equal(@cls, @hash.cls)
-    assert_equal(@ttl, @hash.ttl)
-    assert_equal(@nsdname, @hash.nsdname)
-
-    assert_equal(@str, @string.inspect)
-    assert_equal(@rr_name, @string.name)
-    assert_equal(@type, @string.type)
-    assert_equal(@cls, @string.cls)
-    assert_equal(@ttl, @string.ttl)
-    assert_equal(@nsdname, @string.nsdname)
-    
-    assert_equal(@str, @arr.inspect)
-    assert_equal(@rr_name, @arr.name)
-    assert_equal(@type, @arr.type)
-    assert_equal(@cls, @arr.cls)
-    assert_equal(@ttl, @arr.ttl)
-    assert_equal(@nsdname, @arr.nsdname)
+    @rr_output  = "google.com.             10800   IN      NS      ns1.google.com."
   end
   
-  def test_range
-    assert_raise(RRArgumentError) do 
-      Net::DNS::RR::NS.new(:name => "google.com",
-                          :nsdname => "255.256")
-    end
-    assert_raise(RRArgumentError) do
-      Net::DNS::RR::NS.new(:name => "google.com")
-    end
-    assert_raise(ArgumentError) do
-      Net::DNS::RR::NS.new(Range.new)
-    end
-    assert_raise(RRArgumentError) do
-      Net::DNS::RR::NS.new(Array.new(7))
-    end
-    assert_raise(RRArgumentError) do
-      Net::DNS::RR::NS.new("10800 IN A")
+  
+  def test_initialize_from_hash
+    @record = Net::DNS::RR::NS.new(:name => @rr_name, :nsdname => @rr_nsdname)
+    assert_equal @rr_output,  @record.inspect
+    assert_equal @rr_name,    @record.name
+    assert_equal @rr_type,    @record.type
+    assert_equal @rr_cls,     @record.cls
+    assert_equal @rr_ttl,     @record.ttl
+    assert_equal @rr_nsdname, @record.nsdname
+  end
+  
+  def test_initialize_from_string
+    @record = Net::DNS::RR::NS.new("#{@rr_name} 10800 IN NS #{@rr_nsdname}")
+    assert_equal @rr_output,  @record.inspect
+    assert_equal @rr_name,    @record.name
+    assert_equal @rr_type,    @record.type
+    assert_equal @rr_cls,     @record.cls
+    assert_equal @rr_ttl,     @record.ttl
+    assert_equal @rr_nsdname, @record.nsdname
+  end
+  
+  def test_parse
+    data = "\006google\003com\000\000\002\000\001\000\000*0\000\020\003ns1\006google\003com\000"
+    @record = Net::DNS::RR.parse(data)
+    assert_equal @rr_output,  @record.inspect
+    assert_equal @rr_name,    @record.name
+    assert_equal @rr_type,    @record.type
+    assert_equal @rr_cls,     @record.cls
+    assert_equal @rr_ttl,     @record.ttl
+    assert_equal @rr_nsdname, @record.nsdname
+  end
+  
+  
+  InvalidArguments = [
+    { :name => "google.com", :address => "255.256" },
+    { :name => "google.com" },
+    Object.new,
+    Array.new(7),
+    "10800 IN A",
+  ]
+  
+  InvalidArguments.each_with_index do |arguments, index|
+    define_method "test_initialize_should_raise_with_invalid_arguments_#{index}" do
+      assert_raise(RRArgumentError) { Net::DNS::RR::A.new(arguments) }
     end
   end
+
 end
     
