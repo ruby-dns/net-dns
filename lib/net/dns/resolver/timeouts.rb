8977377b6d18a9965c2cdeed43d714c985a3b4fc
Simone Carletti <weppos@weppos.net>
2009-11-04 22:06:43 +0100

    Timeout class simplification. Removed code remnants and code complexity.

--- lib/net/dns/resolver/timeouts.rb
+++ lib/net/dns/resolver/timeouts.rb
@@ -1,68 +1,72 @@
 require 'timeout'
 
-module SecondsHandle #:nodoc: all
-  def transform(secs)
-    case secs
-    when 0
-      to_s
-    when 1..59
-      "#{secs} seconds"
-    when 60..3559
-      "#{secs/60} minutes and #{secs%60} seconds"
-    else
-      hours = secs/3600
-      secs -= (hours*3600)
-      "#{hours} hours, #{secs/60} minutes and #{secs%60} seconds"
-    end
-  end
-end
 
 class DnsTimeout
-  include SecondsHandle
-  
+
+  attr_reader :seconds
+
+
   def initialize(seconds)
     if seconds.is_a? Numeric and seconds >= 0
-      @timeout = seconds
+      @seconds = seconds
     else
       raise ArgumentError, "Invalid value for tcp timeout"
     end    
   end
-  
+
+  # Returns a string representation of the timeout corresponding
+  # to the number of <tt>@seconds</tt>.
   def to_s
-    if @timeout == 0 
-      @output
+    if @seconds == 0
+      @output.to_s
     else
-      @timeout.to_s
+      @seconds.to_s
     end
   end
   
   def pretty_to_s
-    transform(@timeout)
+    transform(@seconds)
   end
-  
-  def timeout
-    raise LocalJumpError, "no block given" unless block_given?
 
-    if @timeout == 0
-      yield
-    else
-      return Timeout.timeout(@timeout) do
-        yield
+  # Executes the method's block. If the block execution terminates before +sec+
+  # seconds has passed, it returns true. If not, it terminates the execution
+  # and raises Timeout::Error.
+  # If @seconds is 0 or nil, no timeout is set.
+  def timeout(&block)
+    raise LocalJumpError, "no block given" unless block_given?
+    Timeout.timeout(@seconds, &block)
+  end
+
+
+  private
+
+    def transform(secs)
+      case secs
+        when 0
+          to_s
+        when 1..59
+          "#{secs} seconds"
+        when 60..3559
+          "#{secs / 60} minutes and #{secs % 60} seconds"
+        else
+          hours = secs / 3600
+          secs -= (hours * 3600)
+          "#{hours} hours, #{secs / 60} minutes and #{secs % 60} seconds"
       end
     end
-  end
+  
 end
 
 class TcpTimeout < DnsTimeout
   def initialize(seconds)
     @output = "infinite"
-    super(seconds)
+    super
   end
 end
 
 class UdpTimeout < DnsTimeout
   def initialize(seconds)
     @output = "not defined"
-    super(seconds)
+    super
   end
 end
--- test/resolver/timeouts_test.rb
+++ test/resolver/timeouts_test.rb
@@ -2,34 +2,45 @@ require 'test_helper'
 require 'net/dns/resolver/timeouts.rb'
 
 class DnsTimeoutTest < Test::Unit::TestCase
-  
-  def test_timeout_should_raise_localjumperror_without_block
-    assert_raise(LocalJumpError) { DnsTimeout.new(1).timeout }
-  end
-  
-  def test_tcp
-    assert_equal(TcpTimeout.new(0).to_s,"infinite")
-    assert_equal(TcpTimeout.new(30).to_s, "30")
-    assert_equal(TcpTimeout.new(30).pretty_to_s, "30 seconds")
-    assert_equal(TcpTimeout.new(90).pretty_to_s, 
-                 "1 minutes and 30 seconds")
-    assert_equal(TcpTimeout.new(3690).pretty_to_s, 
-                 "1 hours, 1 minutes and 30 seconds")
-    assert_equal(TcpTimeout.new(1).timeout {1+1}, 2)
+
+  def setup
+    @klass = DnsTimeout
   end
 
-  def test_udp
-    assert_equal(UdpTimeout.new(0).to_s,"not defined")
-    assert_equal(UdpTimeout.new(30).to_s, "30")
-    assert_equal(UdpTimeout.new(30).pretty_to_s, "30 seconds")
-    assert_equal(UdpTimeout.new(90).pretty_to_s, 
-                 "1 minutes and 30 seconds")
-    assert_equal(UdpTimeout.new(3690).pretty_to_s, 
-                 "1 hours, 1 minutes and 30 seconds")
-    assert_equal(UdpTimeout.new(1).timeout {1+1}, 2)    
+  def test_initialize
+    timeout = @klass.new(0)
+    assert_instance_of @klass, timeout
   end
-  
-  def test_range_tcp
+
+  def test_initialize_should_set_timeout
+    assert_equal 0, @klass.new(0).seconds
+    assert_equal 10, @klass.new(10).seconds
+  end
+
+  def test_initialize_should_set_raise_with_invalid_timeout
+    assert_raise(ArgumentError) { @klass.new(nil) }
+    assert_raise(ArgumentError) { @klass.new("") }
+    assert_raise(ArgumentError) { @klass.new("foo") }
+    assert_raise(ArgumentError) { @klass.new(-1) }
+  end
+
+
+  def test_to_s
+    assert_equal "", @klass.new(0).to_s
+    assert_equal "1", @klass.new(1).to_s
+    assert_equal "10", @klass.new(10).to_s
+  end
+
+
+  def test_timeout_should_raise_localjumperror_without_block
+    assert_raise(LocalJumpError) { @klass.new(1).timeout }
+  end
+
+end
+
+class TcpTimeoutTest < Test::Unit::TestCase
+
+  def test_initialize
     assert_raise(ArgumentError) do
       TcpTimeout.new("a")
     end
@@ -37,11 +48,34 @@ class DnsTimeoutTest < Test::Unit::TestCase
       TcpTimeout.new(-1)
     end
     assert_raise(TimeoutError) do
-      TcpTimeout.new(0.1).timeout {sleep 2}
+      TcpTimeout.new(0.1).timeout { sleep 2 }
     end
   end
 
-  def test_range_udp
+  def test_to_s
+    assert_equal "infinite", TcpTimeout.new(0).to_s
+    assert_equal "30", TcpTimeout.new(30).to_s
+  end
+
+  def test_pretty_to_s
+    assert_equal "30 seconds", TcpTimeout.new(30).pretty_to_s
+    assert_equal "1 minutes and 30 seconds", TcpTimeout.new(90).pretty_to_s
+    assert_equal "1 hours, 1 minutes and 30 seconds", TcpTimeout.new(3690).pretty_to_s
+  end
+
+  def test_timeout
+    assert_equal 2, TcpTimeout.new(1).timeout { 1 + 1 }    
+  end
+
+  def test_timeout_should_raise_localjumperror_without_block
+    assert_raise(LocalJumpError) { TcpTimeout.new(1).timeout }
+  end
+
+end
+
+class UdpTimeoutTest < Test::Unit::TestCase
+
+  def test_initialize
     assert_raise(ArgumentError) do
       UdpTimeout.new("a")
     end
@@ -53,20 +87,23 @@ class DnsTimeoutTest < Test::Unit::TestCase
     end
   end
 
-end
-
-class TcpTimeoutTest < Test::Unit::TestCase
-  
-  def test_timeout_should_raise_localjumperror_without_block
-    assert_raise(LocalJumpError) { TcpTimeout.new(1).timeout }
+  def test_to_s
+    assert_equal "not defined", UdpTimeout.new(0).to_s
+    assert_equal "30", UdpTimeout.new(30).to_s
   end
-  
-end
 
-class UdpTimeoutTest < Test::Unit::TestCase
-  
+  def test_pretty_to_s
+    assert_equal "30 seconds", UdpTimeout.new(30).pretty_to_s
+    assert_equal "1 minutes and 30 seconds", UdpTimeout.new(90).pretty_to_s
+    assert_equal "1 hours, 1 minutes and 30 seconds", UdpTimeout.new(3690).pretty_to_s
+  end
+
+  def test_timeout
+    assert_equal 2, UdpTimeout.new(1).timeout { 1 + 1 }
+  end
+
   def test_timeout_should_raise_localjumperror_without_block
     assert_raise(LocalJumpError) { UdpTimeout.new(1).timeout }
   end
-  
+
 end
