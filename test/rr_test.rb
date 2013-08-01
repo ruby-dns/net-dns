3e7792d3fea82cbb4384e3d0b84a5e9e11ca6dbe
Chris Lundquist <chris.lundquist@github.com>
2013-08-01 16:09:07 -0700

    [Ruby 2.0.0] add to rr_test too :/

--- test/rr_test.rb
+++ test/rr_test.rb
@@ -121,11 +121,15 @@ class RRTest < Test::Unit::TestCase
     assert_equal(@array, @a_hash.to_a)
   end
 
+  expected_exception_classes = [ArgumentError]
+  # In Ruby 2.0.0 the exception class changes.
+  expected_exception_classes << IPAddr::InvalidAddressError if defined? IPAddr::InvalidAddressError
+
   def test_range
-    assert_raises(ArgumentError) do
+    assert_raises(*expected_exception_classes) do
       Net::DNS::RR.new("google.com. 10800 IM A")
     end
   end
 
 end
-    
+
